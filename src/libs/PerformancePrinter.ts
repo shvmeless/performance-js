9f92c3202ff94609f267e6c26a08eacf23ea7433
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2024-03-25 15:24:27 -0300

    feat: allow to compare results based on a time target

--- src/libs/PerformancePrinter.ts
+++ src/libs/PerformancePrinter.ts
@@ -1,5 +1,6 @@
 //  IMPORTS
 import { type CodePerformance } from './interfaces'
+import { getBorderCharacters, table } from 'table'
 import { dictionary } from '@shvmerc/development'
 import { maths } from '../utils/maths'
 import { stdout } from 'process'
@@ -41,13 +42,20 @@ export class PerformancePrinter {
 
   // METHOD
   public print (options: {
+    target?: string | number
     highlight?: Partial<Record<Color, string>>
   } = {}): void {
 
     const performances = dictionary(this._performances).map((current) => {
       const average = maths.average(...current.values)
-      return { ...current, average }
+      const difference = 0
+      return { ...current, average, difference }
     })
+
+    let target = (typeof options.target === 'number') ? options.target : 0
+    if (typeof options.target === 'string') {
+      if (performances[options.target] !== undefined) target = performances[options.target].average
+    }
 
     const array = dictionary(performances).array((current) => (current))
     array.sort((a, b) => (a.index - b.index))
@@ -59,31 +67,49 @@ export class PerformancePrinter {
 
     const utils = array.reduce((prev, current) => {
       prev.maxAverage = Math.max(prev.maxAverage, current.average)
+      prev.minAverage = Math.min(prev.minAverage, current.average)
       prev.maxNameSize = Math.max(prev.maxNameSize, current.name.length)
       return prev
-    }, { maxAverage: 0, maxNameSize: 0 })
+    }, { minAverage: Infinity, maxAverage: 0, maxNameSize: 0 })
+
+    if (options.target === undefined) target = utils.minAverage
 
     const colors = dictionary(options.highlight ?? {}).remake((name, color) => {
       return [name, color as Color]
     })
 
+    const output: Array<Array<string>> = []
+
     array.forEach((current) => {
 
       const progress = current.average * 100 / utils.maxAverage
+      const difference = maths.round((current.average * 100 / target) - 100)
 
       const highlight = colors[current.name] ?? 'reset'
       const bar = progressBar(progress, 50)
+      const decorator = (difference > 0) ? '+' : (difference < 0) ? '-' : '•'
+      const color = (difference > 0) ? 'red' : (difference < 0) ? 'green' : 'yellow'
 
-      const output: Array<string> = []
-      output.push(current.name.padStart(utils.maxNameSize, ' '))
-      output.push(chalk.bold[highlight as Color](bar))
-      output.push(`${maths.round(current.average, 4)}ms`)
-
-      stdout.write(`${output.join(' ')}\n`)
+      output.push([
+        current.name.padStart(utils.maxNameSize, ' '),
+        chalk.bold[color](decorator),
+        (difference === 0) ? '-' : Math.abs(difference) + '%',
+        `${chalk.bold[highlight](bar)} ${maths.round(current.average, 4)}ms`,
+      ])
 
     })
 
-    stdout.write('\n')
+    stdout.write(table(output, {
+      border: getBorderCharacters('void'),
+      columns: {
+        2: { alignment: 'right' },
+      },
+      columnDefault: {
+        paddingLeft: 0,
+        paddingRight: 1,
+      },
+      drawHorizontalLine: () => false,
+    }) + '\n')
 
   }
 
